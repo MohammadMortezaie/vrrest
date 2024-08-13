8275bc5e4964d8ffbbb4495fb4847a58ced39aa6
MohammadMortezaie <mohammadmortezaei.s@gmail.com>
2024-08-13 10:22:09 -0700

    rm login reg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -174,7 +174,7 @@
 
                             <!-- Authentication Links -->
                             @guest
-                                @if (Route::has('login'))
+                                {{-- @if (Route::has('login'))
                                     <li class="nav-item">
                                         <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                     </li>
@@ -184,7 +184,7 @@
                                     <li class="nav-item">
                                         <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                     </li>
-                                @endif
+                                @endif --}}
                             @else
                                 <li class="nav-item dropdown">
                                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
