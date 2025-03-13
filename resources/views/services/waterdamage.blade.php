1194ec80b264daa8d4185fba16fc3ed5beaaae91
MohammadMortezaie <mohammadmortezaei.s@gmail.com>
2025-03-13 11:00:02 -0700

    add 2 pic in water damage

--- resources/views/services/waterdamage.blade.php
+++ resources/views/services/waterdamage.blade.php
@@ -43,29 +43,42 @@
                 <div class="col-md-4 mb-3">
 
                     <img class="img-fluid rounded" loading="lazy" src="{{ asset('img/water-damage-vr.jpeg') }}"
-                    alt="Water Damage Restoration Vancouver - VR Plus Restoration">
+                        alt="Water Damage Restoration Vancouver - VR Plus Restoration">
 
                 </div>
                 <div class="col-md-8 mb-3">
-                    <h2 class="fs-2 fw-bolder section-title-h1"> {{ __('Water Damage 1-3-1') }}</h2>
+                    <h2 class="mt-3 fs-2 fw-bolder section-title-h1"> {{ __('Water Damage 1-3-1') }}</h2>
                     <p class="lead ">
-                       {{ __('Water Damage 1-3-2') }}
+                        {{ __('Water Damage 1-3-2') }}
                     </p>
                     <div class="text-center mt-3">
                         <strong class="d-block mb-2" style="font-size: 1.2rem;">{{ __('Mold 1-3-7') }} </strong>
-                        <a href="tel:+16048003900" class="btn btn-warning fw-bolder text-uppercase px-4 py-3" style="font-size: 1.0rem; border-radius: 30px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">
+                        <a href="tel:+16048003900" class="btn btn-warning fw-bolder text-uppercase px-4 py-3"
+                            style="font-size: 1.0rem; border-radius: 30px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">
                             📞 Call Now: +1 604-800-3900
                         </a>
-                        <p class="mt-2" style="font-size: 0.95rem; color: #555;">{{ __('Mold 1-3-8') }}  </p>
+                        <p class="mt-2" style="font-size: 0.95rem; color: #555;">{{ __('Mold 1-3-8') }} </p>
                     </div>
 
 
                 </div>
-                <h2 class="fs-2 fw-bolder section-title-h1">
+                <h2 class="mt-5 fs-2 fw-bolder section-title-h1">
                     {{ __('Water Damage 1-4') }}
                 </h2>
                 <p class="lead ">{{ __('Water Damage 1-5') }}
                 </p>
+
+                <div class="col-6">
+                    <img class="img-fluid rounded" loading="lazy" src="{{ asset('img/waterdamage-van.jpeg') }}"
+                        alt="Water Damage Restoration Vancouver - VR Plus Restoration">
+                </div>
+
+                <div class="col-6">
+                    <img class="img-fluid rounded" loading="lazy" src="{{ asset('img/waterdamage-van-front.jpeg') }}"
+                        alt="Water Damage Restoration Vancouver - VR Plus Restoration">
+
+                </div>
+
             </div>
         </div>
     </section>
@@ -339,7 +352,7 @@
                     <hr>
 
                     <h3 class="fs-4 fw-bold">
-                         {{ __('Water Damage 1-46-1') }}
+                        {{ __('Water Damage 1-46-1') }}
                     </h3>
                     <p>
                         {{ __('Water Damage 1-46-2') }}
@@ -347,9 +360,10 @@
 
 
                     <div class="d-flex flex-wrap gap-2 pb-3">
-                        <a href="https://vrrestoration.ca/en/blog/9/what-number-to-call-for-water-damage-in-vancouver" class="btn btn-warning btn-sm">
+                        <a href="https://vrrestoration.ca/en/blog/9/what-number-to-call-for-water-damage-in-vancouver"
+                            class="btn btn-warning btn-sm">
 
-                        {{ __('Water Damage 1-46-3') }}
+                            {{ __('Water Damage 1-46-3') }}
                         </a>
                     </div>
                     <ul class="txt-vr d-flex flex-wrap gap-3">
